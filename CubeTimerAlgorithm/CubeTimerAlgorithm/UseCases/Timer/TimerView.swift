9eb5d74d3114fa16e6c4fe012e13912089e1928d
Jose Guerra <jose.guerra02@inacapmail.cl>
2023-01-16 16:25:57 -0300

    scramble and timer funcionando bien

--- CubeTimerAlgorithm/CubeTimerAlgorithm/UseCases/Timer/TimerView.swift
+++ CubeTimerAlgorithm/CubeTimerAlgorithm/UseCases/Timer/TimerView.swift
@@ -14,7 +14,7 @@ struct TimerView: View {
 
     // Visual components
     @State var info: Bool = false
-    @State var scramble: String = "R' L F R U R2 R' R2 L'2F R'\n                 B R F' L"
+    @State var scrambleSet: String = ""
     
     // Timer variables
     @State var recording: Bool = false
@@ -22,24 +22,27 @@ struct TimerView: View {
     @State var timeColor: Color = .black
     @State var time: String = "0:00"
     
-    // Develop
-    @State var log: String = "log"
+    
     
     var body: some View {
         
-        // Esta parte del codigo controla los Gesture que funcionan sobre el temporizador
+        // Esta parte del codigo controla los Gesture que funcionan sobre el temporizador, Si se mantiene apretado por 0.5 segundos se vuelve verde y al soltarlo pasados los 0.5, el tiempo empieza a correr
         let longPressDrag = LongPressGesture(minimumDuration: 0.5)
             .onEnded { _ in
-               
+                
                 timeColor = .green
                 recording = true
-                log = "recording"
             }
             .sequenced(before: DragGesture(minimumDistance: 0))
             .onEnded { _ in
                 
+                if timeViewModel.mode == .stopped {
+                    timeViewModel.startTimer()
+                } else {
+                    timeViewModel.stopTimer()
+                }
+                
                 recording = false
-                log = "soltado"
             }
         
         
@@ -47,8 +50,9 @@ struct TimerView: View {
             
             VStack {
                 // Un scramble para mixear el cubo
-                Text(scramble)
+                Text(scrambleSet)
                     .font(.system(size: 30))
+                    .multilineTextAlignment(.center)
                 
                 HStack(spacing: 25) {
                     
@@ -58,45 +62,43 @@ struct TimerView: View {
                     
 
                     Button {
-                        
+                        scrambleSet = timeViewModel.getAlgorithm()
                     } label: { RefreshScramble() }
                     
                     
 
                 }
-                Text("\(log)")
-                    .foregroundColor(.red)
+                
+            }
+            .onAppear {
+                scrambleSet = timeViewModel.getAlgorithm()
             }
             .sheet(isPresented: $info) {
                 InfoView()
             }
             
-                        
             
             Text("\(String(format: "%.2f", timeViewModel.time))")
+                // Este tapGesture permite que cambie el color del timer, si se apreta rapidamente y esta en 0.00 se pone en rojo por 1 segundo, y si el tiempo esta corriendo lo para
                 .onTapGesture {
                     
-                    if timeViewModel.mode == .stopped {
-                        timeViewModel.startTimer()
-                    } else {
+                    if timeViewModel.mode == .recording {
                         timeViewModel.stopTimer()
                     }
                     
-                    
-                    
-                    log = "tapped"
                     timeColor = .red
                     tapped = true
                     
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
                         tapped = false
                         timeColor = .black
+                        
                     }
                 
                 }
                 .gesture(longPressDrag)
-                .font(.system(size: 100))
-                .padding(.bottom, 240)
+                .font(.system(size: 120))
+                .padding(.bottom, 200)
                 .foregroundColor(timeColor)
                 
             
--- CubeTimerAlgorithm/CubeTimerAlgorithm/UseCases/Timer/TimerViewModel.swift
+++ CubeTimerAlgorithm/CubeTimerAlgorithm/UseCases/Timer/TimerViewModel.swift
@@ -15,9 +15,12 @@ class TimerViewModel: ObservableObject {
     var lastTime: Double = 0.0
     var timer = Timer()
     
+    var scrambleLenght: Int = 15
+    
     
     func startTimer() {
         
+        time = 0.0
         self.mode = .recording
         timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) { timer in
             self.time += 0.1
@@ -32,10 +35,54 @@ class TimerViewModel: ObservableObject {
         
     }
     
+    func getAlgorithm() -> String {
+        
+        var algorithm: [String] = []
+        let moves = ["L", "R", "F", "U", "B", "D"]
+        var last = ""
+        var posibilities: [String] = []
+        var returnableScramble: String = ""
+        var counter = 0
+
+        for _ in 1...scrambleLenght {
+            
+            posibilities = []
+            var move = moves.randomElement()!
+                
+            while move == last {
+              move = moves.randomElement()!
+            }
+                
+            posibilities.append(contentsOf: ["\(move)", "\(move)", "\(move)'", "2\(move)"])
+            algorithm.append(posibilities.randomElement()!)
+            last = move
+            
+        }
+
+        
+        /// Edit: Para retornar se podrian tomar el numero de caracteres totales en el [String] y de ahi poner el \n
+        for i in algorithm {
+            counter += 1
+            
+            if counter == (scrambleLenght / 2) + 3 {
+                returnableScramble += ("\(i)\n ")
+            } else {
+                returnableScramble += ("\(i) ")
+            }
+            
+        }
+        
+        return returnableScramble
+      
+    }
+
+    
     
     enum timerMode {
         case recording
         case stopped
     }
+    
+    
     
 }
